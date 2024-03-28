310a9cfef2bae1e78b39cf9655f8c35bf609eb40
David Whalley <david.whalley@greenwoodcampbell.com>
2024-03-28 14:36:31 +0000

    Minor Style changes and metadata change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from 'next/font/google';
+import openGraphImage  from "./assets/images/main-image.jpg";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -12,6 +13,15 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Ortheyus | Software Developer | Web Enthusiast",
   description: "Explore the portfolio of Ortheyus, a software developer passionate about creating innovative solutions. Discover projects, skills, and expertise in web development and programming.",
+  keywords: "ortheyus, software developer, web developer, web enthusiast, portfolio, projects, skills, expertise, web development, programming, software development, web design, web technologies, web applications, web services, web solutions, web projects, web tools, web frameworks, web libraries, web languages, web standards, web practices, web techniques, web methodologies, web patterns, web principles, web concepts, web ideas, web trends, web insights, web resources, web references, web tutorials, web guides, web tips, web tricks, web hacks, web cheats, web examples, web demos, web showcases, web experiments, web explorations, web adventures, web experiences, web journeys, web stories, web narratives, web legends, web myths, web tales, web fables, web sagas, web epics, web chronicles, web histories, web biographies, web profiles, web resumes, web CVs, web applications, web services, web solutions, web projects, web tools, web frameworks, web libraries, web languages, web standards, web practices, web techniques, web methodologies, web patterns, web principles, web concepts, web ideas, web trends, web insights, web resources, web references, web tutorials, web guides, web tips, web tricks, web hacks, web cheats, web examples, web demos, web showcases, web experiments, web explorations, web adventures, web experiences, web journeys, web stories, web narratives, web legends, web myths, web tales, web fables, web sagas, web epics, web chronicles, web histories, web biographies, web profiles, web resumes, web CVs, web applications, web services, web solutions, web projects, web tools, web frameworks, web libraries, web languages, web standards, web practices, web techniques, web methodologies, web patterns, web principles, web concepts, web ideas, web trends, web insights, web resources, web references, web tutorials, web guides, web tips, web tricks, web hacks, web cheats, web examples, web demos, web showcases, web experiments, web explorations, web adventures, web experiences, web journeys, web stories, web narratives, web legends, web myths, web tales, web fables, web sagas, web epics, web chronicles, web histories, web biographies, web profiles, web resumes, web CVs, web applications, web services, web solutions, web projects, web tools, web frameworks, web libraries, web languages, web standards, web practices, web techniques, web methodologies, web patterns, web principles, web concepts, web ideas, web trends, web insights, web resources, web references, web tutorials, web guides, web tips, web tricks, web hacks, web cheats, web examples, web demos, web showcases, web experiments, web explorations, web adventures, web experiences, web journeys, web stories, web narratives, web legends, web",
+  openGraph: {
+    type: "website",
+    locale: "en_UK",
+    url: "https://next-portfolio-delta-snowy.vercel.app/",
+    title: "Ortheyus | Software Developer | Web Enthusiast",
+    description: "Explore the portfolio of Ortheyus, a software developer passionate about creating innovative solutions. Discover projects, skills, and expertise in web development and programming.",
+    ...openGraphImage,
+  },
 };
 
 export default function RootLayout({
