5329b2f6d5241df256a5e8a2cc5a6d036289a426
David Whalley <77077742+dwhalley15@users.noreply.github.com>
2024-07-13 10:53:10 +0100

    Added projects image

--- src/app/services/imageService/imageService.tsx
+++ src/app/services/imageService/imageService.tsx
@@ -1,4 +1,5 @@
 import theGuitarShopImage from "../../../../public/the-guitar-shop-electric.png";
+import inspirationalQuotesImage from "../../../../public/inspirational-quotes-home.png";
 import imageNotFound from "../../../../public/image-not-found.jpg";
 import Image from "next/image";
 
@@ -6,6 +7,8 @@ export default function ImageService(imageName: string, altText: string){
     switch(imageName.toLowerCase()){
         case 'theguitarshop':
             return <Image src={theGuitarShopImage} alt={altText} width={426} height={240}/>;
+        case 'inspirationqutoes':
+            return <Image src={inspirationalQuotesImage} alt={altText} width={426} height={240}/>;
         default: <Image src={imageNotFound} alt={altText} width={426} height={240}/>;
     }
 }
