ac2519bf6d4da9efc08a66d35b363f3e0fa6a86a
David Whalley <77077742+dwhalley15@users.noreply.github.com>
2024-09-20 07:53:56 +0100

    Updated next due to vulnerability redesigned footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -30,42 +30,43 @@ export default function Footer({ socialLinks, navLinks, projects }: FooterProps)
   return (
     <>
       <footer className="footer">
-
-        <div className="footer-nav">
-          <div className="footer-image">
-            <Image src={mainImage} alt="Image of a red light bulb with a black background" width={600} height={600} />
-          </div>
-          <div className="footer-lists">
-            <ul>
-              <li><h2>Site Map</h2></li>
-              {navLinks.map((link: NavbarItem) => (
-                <li key={link.id}>
-                  <Link href={link.link_name === 'home' ? '/' : `/${link.link_name}`}>{link.link_name.charAt(0).toUpperCase() + link.link_name.slice(1)}</Link>
-                </li>
-              ))}
-            </ul>
-            <ul>
-              <li><h2>Project Map</h2></li>
-              {projects.map((project: ProjectProps, index) => (
-                <li key={index}>
-                  <Link href={`/projects/${project.url}`}>{project.title}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="footer-social-copyright">
-          <div className="social">
+          <div className="footer-left">
+            <div className="footer-image">
+              <Link href={"/"}>
+                <Image src={mainImage} alt="Image of a red light bulb with a black background" width={600} height={600} />
+              </Link>
+            </div>
+            <div className="social">
             {socialLinks.map((link: SocialLinkItem) => (
               <Link key={link.id} href={link.social_url} target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={getFontAwesomeIcon(link.social_name)} size="1x" />
               </Link>
             ))}
+            </div>
           </div>
-          <p className="copyright">
-            {`Copyright ${currentYear} \u00A9 Ortheyus | All Rights Reserved`}
-          </p>
-        </div>
+          <div className="footer-right">
+            <div className="footer-lists">
+              <ul>
+                <li><h2>Site Map</h2></li>
+                {navLinks.map((link: NavbarItem) => (
+                  <li key={link.id}>
+                    <Link href={link.link_name === 'home' ? '/' : `/${link.link_name}`}>{link.link_name.charAt(0).toUpperCase() + link.link_name.slice(1)}</Link>
+                  </li>
+                ))}
+              </ul>
+              <ul>
+                <li><h2>Project Map</h2></li>
+                {projects.map((project: ProjectProps, index) => (
+                  <li key={index}>
+                    <Link href={`/projects/${project.url}`}>{project.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <p className="copyright">
+              {`Copyright ${currentYear} \u00A9 Ortheyus | All Rights Reserved`}
+            </p>
+          </div>
       </footer>
     </>
   );
