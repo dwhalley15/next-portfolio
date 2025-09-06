a4a9fc8138c7a351da6e1f275904ff78cb23d5c7
dwhalley15 <one_winged@hotmail.com>
2025-09-06 18:13:00 +0100

    dev added a number of new project images

--- src/app/services/imageService/imageService.tsx
+++ src/app/services/imageService/imageService.tsx
@@ -1,7 +1,13 @@
 import theGuitarShopImage from "../../../../public/the-guitar-shop-electric.png";
+import theGuitarShopHome from "../../../../public/the-guitar-shop-home.png";
+import theGuitarShopMobile from "../../../../public/the-guitar-shop-mobile.png";
 import inspirationalQuotesImage from "../../../../public/inspirational-quotes-home.png";
 import rightTrackImage from "../../../../public/right-track-image.png";
+import rightTrackCourses from "../../../../public/right-track-courses.png";
+import rightTrackNewTask from "../../../../public/right-track-new-task.png";
 import dailyGrindImage from "../../../../public/daily-grind-account.png";
+import dailyGrindActivity from "../../../../public/daily-grind-activity.png";
+import dailyGrindProfile from "../../../../public/daily-grind-profile.png";
 import battleshipsImage from "../../../../public/battleships-home.png";
 import instaTranslateImage from "../../../../public/insta-translate.png";
 import instaTranslateListingImage from "../../../../public/insta-translate-listing.jpg";
@@ -12,8 +18,16 @@ import theGuitarShopListingImage from "../../../../public/the-guitar-shop-listin
 import rightTrackListingImage from "../../../../public/right-track-listing.jpg";
 import appleGameImage from "../../../../public/apple-catcher-game.jpg";
 import appleGameListingImage from "../../../../public/apple-game-listing.jpg";
+import appleGameImageInGame from "../../../../public/apple-catcher-image-in-game.png";
 import portfolioImage from "../../../../public/portfolio-home.png";
+import portfolioMobile from "../../../../public/portfolio-mobile.png";
+import portfolioProjects from "../../../../public/portfolio-projects.png";
 import portfolioListingImage from "../../../../public/portfolio-listing.jpg";
+import medicalPortfoliosHome from "../../../../public/medical-portfolios-home.png";
+import medicalPortfoliosListing from "../../../../public/medical-portfolios-listing.jpg";
+import medicalPortfoliosSearch from "../../../../public/medical-portfolios-search.png";
+import medicalPortfoliosPortfolio from "../../../../public/medical-portfolios-portfolio.png";
+import medicalPortfoliosMobile from "../../../../public/medical-portfolios-mobile.png";
 import imageNotFound from "../../../../public/image-not-found.jpg";
 import Image, { StaticImageData } from "next/image";
 
@@ -21,9 +35,15 @@ type ImageMap = Record<string, StaticImageData>;
 
 const imageMap: ImageMap = {
     theguitarshop: theGuitarShopImage,
+    theguitarshophome: theGuitarShopHome,
+    theguitarshopmobile: theGuitarShopMobile,
     inspirationqutoes: inspirationalQuotesImage,
     righttrack: rightTrackImage,
+    righttrackcourses: rightTrackCourses,
+    righttracknewtask: rightTrackNewTask,
     dailygrind: dailyGrindImage,
+    dailygrindactivity: dailyGrindActivity,
+    dailygrindprofile: dailyGrindProfile,
     battleships: battleshipsImage,
     instatranslate: instaTranslateImage,
     instatranslatelisting: instaTranslateListingImage,
@@ -36,6 +56,14 @@ const imageMap: ImageMap = {
     portfolio: portfolioImage,
     portfoliolisting: portfolioListingImage,
     applegamelisting: appleGameListingImage,
+    applegameingame: appleGameImageInGame,
+    portfoliomobile: portfolioMobile,
+    portfolioprojects: portfolioProjects,
+    medicalportfolioshome: medicalPortfoliosHome,
+    medicalportfolioslisting: medicalPortfoliosListing,
+    medicalportfoliossearch: medicalPortfoliosSearch,
+    medicalportfoliosportfolio: medicalPortfoliosPortfolio,
+    medicalportfoliosmobile: medicalPortfoliosMobile,
     default: imageNotFound,
   };
 
